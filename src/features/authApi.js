56bc88c896dff0343bc900e7581ad621f3709a41
thangavelpandi01 <tthangavelpandi11@gmail.com>
2026-04-15 11:40:53 +0530

    Update authApi.js

--- src/features/authApi.js
+++ src/features/authApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://saas-backend-1-eia8.onrender.com/api", 
 });
 
 // ✅ Attach token automatically
